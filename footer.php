19abbe432bce507c4ec5cffeb6c5914300642f76
Francine PAMPANA <pampanafrancine@gmail.com>
2026-03-14 11:19:26 +0100

    init

---
+++ footer.php
@@ -0,0 +1,21 @@
+<footer>
+
+<h3>Mentions légales</h3>
+
+<p>
+Nom : UpcycleConnect
+</p>
+
+<p>
+Siège social : 174 rue La Fayette, 75010 Paris
+</p>
+
+<p>
+Email : contact@upcycleconnect.fr
+</p>
+
+<p>
+Hébergeur : OVH
+</p>
+
+</footer>
