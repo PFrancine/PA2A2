b770b6b6aa84c68ce71923020d11f0bfbc99721b
Francine PAMPANA <f.pampana@myskoale.fr>
2026-03-15 22:51:39 +0100

    testredirection

--- index.php
+++ index.php
@@ -172,7 +172,7 @@ FORUM
     <?php endforeach; ?>
     <h3>Question du jour</h3>
     <p>Que peut-on faire avec des bouteilles en verre recyclées ?</p>
-    <a href="connexion.php" class="btn">Accéder au Forum</a>
+    <a href="auth/connexion.php" class="btn">Accéder au Forum</a>
 </section>
 
 <!-- =========================
