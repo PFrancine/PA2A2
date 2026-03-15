85c3c4eefc0549148554baf777b0849e15cb54fe
Francine PAMPANA <f.pampana@myskoale.fr>
2026-03-15 21:36:09 +0100

    testconnexok

--- auth/connexion.php
+++ auth/connexion.php
@@ -38,7 +38,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Connexion - UpcycleConnect</title>
-    <link rel="stylesheet" href="style_connexion.css">
+    <link rel="stylesheet" href="style_connexion.css?v=999">
 </head>
 <body>
 
