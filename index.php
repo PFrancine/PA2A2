6453aa2dac62cbc70d8bb3d19c9f1be0ed6cf1d5
Francine PAMPANA <f.pampana@myskoale.fr>
2026-03-29 21:49:37 +0200

    testtxtcenter

--- index.php
+++ index.php
@@ -70,7 +70,7 @@ $annonces = $pdo->query($sqlForum)->fetchAll();
 </section>
 
 <section class="presentation" id="presentation">
-    <h2>Découvrer UpCycle</h2>
+    <h2>Découvrez UpCycle</h2>
     <p>
     Chez UpcycleConnect, nous sommes dédiés à la valorisation des matériaux et à la réduction des déchets par l’upcycling.
     </p>
@@ -108,6 +108,7 @@ $annonces = $pdo->query($sqlForum)->fetchAll();
 
 
 <?php 
+
 /* formation dynamique
 if($formation): ?>
         <div class="card">
